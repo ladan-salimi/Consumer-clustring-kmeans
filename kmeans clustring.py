736b97b52d22717ce0b2456a7746c77dc92fe2fe
Ladan Salimi <55614277+ladan-salimi@users.noreply.github.com>
2022-08-01 14:37:23 +0430

    Create kmeans clustring.py

---
+++ kmeans clustring.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+from sklearn.cluster import KMeans
+
+df = pd.read_excel (r'C:\Users\ladan\Desktop\clustring\input2018.xlsx')
+#Selecting the feature: consumption value and country
+x = df.iloc[:,1:2]
+#Apply K-means Clustering
+kmeans = KMeans(2)
+kmeans.fit(x)
+#Clustering Results:prediction of clusters for each country
+identified_clusters = kmeans.fit_predict(x)
+#creates a copy of an existing list
+data_with_clusters = df.copy()
+#adding "Clusters" columns to the end of original dataset to put the number of clusters related to each country on it
+data_with_clusters['Clusters'] = identified_clusters
+plt.scatter(data_with_clusters['country'],data_with_clusters['value'],c=data_with_clusters['Clusters'],cmap='rainbow')
